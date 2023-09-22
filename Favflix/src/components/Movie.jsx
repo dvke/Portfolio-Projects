26eb9cffd5886026c3dd3078c601dec8768ae8fd
Uche Obiekwe <obiekweuchenna@gmail.com>
2023-09-22 12:06:41 +0100

    save movie feature

--- Favflix/src/components/Movie.jsx
+++ Favflix/src/components/Movie.jsx
@@ -1,8 +1,31 @@
 import { useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { useAuth } from "../context/AuthContext";
+import { db } from "../firebase";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
 
 const Movie = ({ item }) => {
   const [like, setLike] = useState(false);
+  const [saved, setSaved] = useState(false);
+  const { user } = useAuth();
+
+  const movieID = doc(db, "users", `${user?.email}`);
+  // save movie
+  const saveMovie = async () => {
+    if (user?.email) {
+      setLike(!like);
+      setSaved(true);
+      await updateDoc(movieID, {
+        savedMovies: arrayUnion({
+          id: item.id,
+          title: item.title,
+          img: item.backdrop_path,
+        }),
+      });
+    } else {
+      alert("Please log in to save a movie");
+    }
+  };
 
   return (
     <>
@@ -16,7 +39,7 @@ const Movie = ({ item }) => {
           <p className="white-space-norma text-xs flex font-bold justify-center items-center h-full text-center">
             {item?.title}
           </p>
-          <p>
+          <p className="border border-red-300" onClick={saveMovie}>
             {like ? (
               <FaHeart className="absolute top-4 left-4 text-gray-200" />
             ) : (
