b49c2309c6ef9d7b204c1ab1579049672e799172
Uche Obiekwe <obiekweuchenna@gmail.com>
2023-09-22 13:31:12 +0100

    watchlist on account page

---
+++ Favflix/src/components/SavedMovies.jsx
@@ -0,0 +1,94 @@
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { useState, useEffect, useRef } from "react";
+import { useAuth } from "../context/AuthContext";
+import { db } from "../firebase";
+import { updateDoc, doc, onSnapshot } from "firebase/firestore";
+import { AiOutlineClose } from "react-icons/ai";
+
+const SavedMovies = () => {
+  const slider = useRef();
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const { user } = useAuth();
+
+  //Slider functions
+  const slideRight = () => {
+    slider.current.scrollLeft = slider.current.scrollLeft + 500;
+  };
+
+  const slideLeft = () => {
+    slider.current.scrollLeft = slider.current.scrollLeft - 500;
+  };
+
+  const movieRef = doc(db, "users", `${user.email}`);
+
+  const deleteMovie = async (id) => {
+    try {
+      const result = movies.filter((movie) => movie.id !== id);
+      await updateDoc(movieRef, {
+        savedMovies: result,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    onSnapshot(movieRef, (doc) => {
+      setMovies(doc.data()?.savedMovies);
+    });
+    setIsLoading(false);
+  }, [user?.email]);
+
+  return (
+    <>
+      <h2 className="text-white font-bold md:text-xl p-4">Watchlist</h2>
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          onClick={slideLeft}
+          size={30}
+          className="bg-white absolute left-0 rounded-full opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
+        <div
+          ref={slider}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide"
+        >
+          {isLoading ? (
+            <p className="text-white text-sm">loading...</p>
+          ) : (
+            movies.map((movie, id) => (
+              <div
+                key={movie.id}
+                className="w-[170px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2"
+              >
+                <img
+                  className="w-full h-auto block"
+                  src={`https://image.tmdb.org/t/p/w500/${movie?.img}`}
+                  alt={movie?.title}
+                />
+                <div className="absolute transition-all duration-300 ease-in-out top-0 left-0 h-full w-full hover:bg-black/60 opacity-0 hover:opacity-100 text-white">
+                  <p className="white-space-norma text-xs flex font-bold justify-center items-center h-full text-center">
+                    {movie?.title}
+                  </p>
+                  <p
+                    onClick={() => deleteMovie(movie.id)}
+                    className="absolute text-gray-300 top-4 right-4"
+                  >
+                    <AiOutlineClose />
+                  </p>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
+        <MdChevronRight
+          onClick={slideRight}
+          size={30}
+          className="bg-white absolute right-0 rounded-full opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
+      </div>
+    </>
+  );
+};
+
+export default SavedMovies;
--- Favflix/src/pages/Account.jsx
+++ Favflix/src/pages/Account.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import SavedMovies from "../components/SavedMovies";
 
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <>
+      <div className="w-full text-white">
+        <img
+          className=" w-full h-[400px ] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/dc1cf82d-97c9-409f-b7c8-6ac1718946d6/bd6666ad-90f8-453d-ac3d-25092f05db36/NG-en-20230911-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="/"
+        />
+
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]">
+          <div className="absolute top-[20%] p-4 md:p-8">
+            <h1 className="text-3xl md:text-5xl font-bold">My Movies</h1>
+          </div>
+        </div>
+      </div>
+      <SavedMovies />
+    </>
+  );
 };
 
 export default Account;
