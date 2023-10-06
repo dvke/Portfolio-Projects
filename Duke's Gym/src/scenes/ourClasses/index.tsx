2928c6b929ee098dac8b0bf7b9f8334b9f79ac69
Uche Obiekwe <obiekweuchenna@gmail.com>
2023-10-06 22:19:00 +0100

    class type and component

--- Duke's Gym/src/scenes/ourClasses/index.tsx
+++ Duke's Gym/src/scenes/ourClasses/index.tsx
@@ -1,7 +1,41 @@
+import classes from "@/data/classes";
+import Header from "@/shared/Header";
+import { ClassType } from "@/shared/types";
+import { motion } from "framer-motion";
+
 type Props = {};
 
 const OurClasses = (props: Props) => {
-  return <section id="ourclasses">OurClasses</section>;
+  return (
+    <section id="ourclasses" className="w-full py-40 bg-primary-100">
+      <motion.div
+        className="mx-auto w-5/6"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, x: -50 },
+          visible: { opacity: 1, x: 0 },
+        }}
+      >
+        <div className="md:w-3/5">
+          <Header>our classes</Header>
+          <p className="py-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            accusantium quo nisi eveniet. Inventore illo voluptates obcaecati,
+            doloremque, culpa illum qui optio doloribus odio corrupti aspernatur
+            aliquid veniam! At, sequi?
+          </p>
+        </div>
+      </motion.div>
+      <div className="mt-10 h-[350px] overflow-x-auto overflow-y-hidden">
+        <ul className="w-[28000px] whitespace-nowrap">
+          {classes.map((item: ClassType) => {})}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default OurClasses;
