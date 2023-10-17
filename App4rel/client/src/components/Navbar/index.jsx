dba8af41086d99d5ea3c9403689bb82e88253666
Uche Obiekwe <obiekweuchenna@gmail.com>
2023-10-17 12:53:48 +0100

    install react-icons

--- App4rel/client/src/components/Navbar/index.jsx
+++ App4rel/client/src/components/Navbar/index.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { IoIosArrowDown, IoIosSearch, IoMdHeartEmpty } from "react-icons/io";
+import { IoCartOutline } from "react-icons/io5";
+import { BiUser } from "react-icons/bi";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className="">
+        <a href="">Help & FAQS</a>
+        <a href="">
+          <img src="./public/flag_ng.png" alt="ng-flag" />
+        </a>
+      </div>
+      <div></div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- App4rel/client/src/pages/ErrorPage/index.jsx
+++ App4rel/client/src/pages/ErrorPage/index.jsx
@@ -7,17 +7,10 @@ export default function ErrorPage() {
 
   return (
     <div
-      className="flex flex-col gap-7 items-center justify-center h-[100vh]"
+      className=" flex flex-col gap-7 items-center justify-center h-[100vh]"
       id="error-page"
     >
-      <h1 className="text-red-600 text-[3rem] font-bold">Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i className="text-slate-500">
-          {// @ts-ignore
-          error.statusText || error.message}
-        </i>
-      </p>
+      <img src="./public/img/ErrorPageGraphic.avif" alt="error-graphic" />
     </div>
   );
 }
