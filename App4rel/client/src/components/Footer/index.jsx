534366dc1fade2afa153f19f3d99a08bb6fd31be
Uche Obiekwe <obiekweuchenna@gmail.com>
2023-10-18 02:04:33 +0100

    footer design

--- App4rel/client/src/components/Footer/index.jsx
+++ App4rel/client/src/components/Footer/index.jsx
@@ -1,7 +1,77 @@
 import React from "react";
+import {
+  BiLogoFacebookCircle,
+  BiLogoInstagramAlt,
+  BiLogoSnapchat,
+} from "react-icons/bi";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="absolute bottom-0">
+      {/* top */}
+      <div className="border w-[100vw] py-3 flex items-center g justify-center">
+        {/* social */}
+        <div className="border-r-2 pr-10 flex gap-5 text-[1.45rem]">
+          <i className="cursor-pointer hover:text-[#3c5a99]">
+            <BiLogoFacebookCircle />
+          </i>
+          <i className="cursor-pointer hover:text-[#8433b7]">
+            <BiLogoInstagramAlt />
+          </i>
+          <i className="cursor-pointer hover:text-[#ffd502]">
+            <BiLogoSnapchat />
+          </i>
+        </div>
+        {/* payment */}
+        <div className="pl-10 w-[300px]">
+          <img src="../public/img/payment.png" alt="payment-options" />
+        </div>
+      </div>
+      {/* middle */}
+      <div className="flex justify-around px-10 py-10 bg-[#f8f8f8] text-gray-500">
+        {/* categories */}
+        <div>
+          <h3 className="font-bold uppercase">Categories</h3>
+          <ul className="text-sm flex flex-col gap-3">
+            <li className="hover:text-blue-400">Women</li>
+            <li className="hover:text-blue-400">Men</li>
+            <li className="hover:text-blue-400">Accessories</li>
+            <li className="hover:text-blue-400">New Arrivals</li>
+          </ul>
+        </div>
+        {/* links */}
+        <div>
+          <h3 className="font-bold uppercase">Links</h3>
+          <ul className="text-sm flex flex-col gap-3">
+            <li className="hover:text-blue-400">FAQ</li>
+            <li className="hover:text-blue-400">Pages</li>
+            <li className="hover:text-blue-400">Stores</li>
+            <li className="hover:text-blue-400">Cookies</li>
+          </ul>
+        </div>
+        {/* about */}
+        <div className="w-1/4">
+          <h3 className="font-bold uppercase">About</h3>
+          <p className="text-sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+            repudiandae voluptate dolore voluptatem repellat optio?
+          </p>
+        </div>
+        {/* contact */}
+        <div>
+          <h3 className="font-bold uppercase">Contact</h3>
+          <p className="text-sm">080 1234 56789</p>
+        </div>
+      </div>
+      {/* bottom */}
+      <div>
+        <div className="text-sm text-center p-2 bg-[#dddddd]">
+          {" "}
+          &#169; APP4R3L by Uche Obiekwe 🤓
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
